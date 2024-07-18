76786461873b37a85f6207f5af9d08100668da89
Yasin ilhan <yasinilhan61@gmail.com>
2024-07-18 20:16:30 +0300

    Update screen_time_api_ios.dart

--- lib/screen_time_api_ios.dart
+++ lib/screen_time_api_ios.dart
@@ -1,4 +1,3 @@
-
 import 'screen_time_api_ios_platform_interface.dart';
 import 'screen_time_api_ios_method_channel.dart';
 
@@ -15,5 +14,20 @@ class ScreenTimeApiIos {
   Future encourageAll() async {
     final instance = ScreenTimeApiIosPlatform.instance as MethodChannelScreenTimeApiIos;
     await instance.encourageAll();
+  }
+
+  Future disallowApps(List<String> bundleIdentifiers) async {
+    final instance = ScreenTimeApiIosPlatform.instance as MethodChannelScreenTimeApiIos;
+    await instance.disallowApps(bundleIdentifiers);
+  }
+
+  Future addDisallowedApps(List<String> bundleIdentifiers) async {
+    final instance = ScreenTimeApiIosPlatform.instance as MethodChannelScreenTimeApiIos;
+    await instance.addDisallowedApps(bundleIdentifiers);
+  }
+
+  Future removeDisallowedApps(List<String> bundleIdentifiers) async {
+    final instance = ScreenTimeApiIosPlatform.instance as MethodChannelScreenTimeApiIos;
+    await instance.removeDisallowedApps(bundleIdentifiers);
   }
 }
