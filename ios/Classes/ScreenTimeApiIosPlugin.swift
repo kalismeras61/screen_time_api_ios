0f53ab191a0391c52251d9551373ba2f9af95521
Yasin ilhan <yasinilhan61@gmail.com>
2024-07-18 20:15:37 +0300

    Update ScreenTimeApiIosPlugin.swift

--- ios/Classes/ScreenTimeApiIosPlugin.swift
+++ ios/Classes/ScreenTimeApiIosPlugin.swift
@@ -9,40 +9,59 @@ public class ScreenTimeApiIosPlugin: NSObject, FlutterPlugin {
         let instance = ScreenTimeApiIosPlugin()
         registrar.addMethodCallDelegate(instance, channel: channel)
     }
-    
+
     public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
         switch call.method {
         case "selectAppsToDiscourage":
             Task {
-                // スクリーンタイムAPIの認証
+                // Screen Time API authorization
                 try await FamilyControlModel.shared.authorize()
                 showController()
             }
             result(nil)
         case "encourageAll":
-            // 全部解放する
-            FamilyControlModel.shared.encourageAll();
+            // Release all restrictions
+            FamilyControlModel.shared.encourageAll()
             FamilyControlModel.shared.saveSelection(selection: FamilyActivitySelection())
             result(nil)
+        case "disallowApps":
+            if let args = call.arguments as? [String] {
+                FamilyControlModel.shared.disallowApps(bundleIdentifiers: args)
+                result(nil)
+            } else {
+                result(FlutterError(code: "INVALID_ARGUMENT", message: "Expected a list of bundle identifiers", details: nil))
+            }
+        case "addDisallowedApps":
+            if let args = call.arguments as? [String] {
+                FamilyControlModel.shared.addDisallowedApps(bundleIdentifiers: args)
+                result(nil)
+            } else {
+                result(FlutterError(code: "INVALID_ARGUMENT", message: "Expected a list of bundle identifiers", details: nil))
+            }
+        case "removeDisallowedApps":
+            if let args = call.arguments as? [String] {
+                FamilyControlModel.shared.removeDisallowedApps(bundleIdentifiers: args)
+                result(nil)
+            } else {
+                result(FlutterError(code: "INVALID_ARGUMENT", message: "Expected a list of bundle identifiers", details: nil))
+            }
         default:
             result(FlutterMethodNotImplemented)
         }
     }
-    
-    @objc func onPressClose(){
+
+    @objc func onPressClose() {
         dismiss()
     }
-    
+
     func showController() {
         DispatchQueue.main.async {
             let scenes = UIApplication.shared.connectedScenes
             let windowScene = scenes.first as? UIWindowScene
             let windows = windowScene?.windows
-            let controller = windows?.filter({ (w) -> Bool in
-                return w.isHidden == false
-            }).first?.rootViewController as? FlutterViewController
-            
-            // アプリ選択のUIを出す
+            let controller = windows?.filter { !$0.isHidden }.first?.rootViewController as? FlutterViewController
+
+            // Display the app selection UI
             let selectAppVC: UIViewController = UIHostingController(rootView: ContentView())
             selectAppVC.navigationItem.rightBarButtonItem = UIBarButtonItem(
                 barButtonSystemItem: .close,
@@ -53,15 +72,13 @@ public class ScreenTimeApiIosPlugin: NSObject, FlutterPlugin {
             controller?.present(naviVC, animated: true, completion: nil)
         }
     }
-    
-    func dismiss(){
+
+    func dismiss() {
         DispatchQueue.main.async {
             let scenes = UIApplication.shared.connectedScenes
             let windowScene = scenes.first as? UIWindowScene
             let windows = windowScene?.windows
-            let controller = windows?.filter({ (w) -> Bool in
-                return w.isHidden == false
-            }).first?.rootViewController as? FlutterViewController
+            let controller = windows?.filter { !$0.isHidden }.first?.rootViewController as? FlutterViewController
             controller?.dismiss(animated: true, completion: nil)
         }
     }
