5875d088788fa88782e8b07b5c0ab40821a211f5
kalismeras61 <yasinilhan61@gmail.com>
2024-07-19 08:45:37 +0300

    fix issue

--- ios/Classes/FamilyControlModel.swift
+++ ios/Classes/FamilyControlModel.swift
@@ -66,11 +66,20 @@ class FamilyControlModel: ObservableObject {
         return try? decoder.decode(FamilyActivitySelection.self, from: data)
     }
 
+    // Utility function to create ApplicationToken
+    private func createApplicationToken(bundleIdentifier: String) -> ApplicationToken? {
+        // Attempting to create ApplicationToken with the correct initializer
+        guard let bundleIdentifier = BundleIdentifier(bundleIdentifier) else {
+            return nil
+        }
+        return ApplicationToken(from: bundleIdentifier)
+    }
+
     // New function to disallow specific apps
     func disallowApps(bundleIdentifiers: [String]) {
         var applicationTokens = Set<ApplicationToken>()
         for bundleIdentifier in bundleIdentifiers {
-            if let token = ApplicationToken(bundleIdentifier: bundleIdentifier) {
+            if let token = createApplicationToken(bundleIdentifier: bundleIdentifier) {
                 applicationTokens.insert(token)
             }
         }
@@ -81,7 +90,7 @@ class FamilyControlModel: ObservableObject {
     func addDisallowedApps(bundleIdentifiers: [String]) {
         var applicationTokens = store.shield.applications ?? Set<ApplicationToken>()
         for bundleIdentifier in bundleIdentifiers {
-            if let token = ApplicationToken(bundleIdentifier: bundleIdentifier) {
+            if let token = createApplicationToken(bundleIdentifier: bundleIdentifier) {
                 applicationTokens.insert(token)
             }
         }
@@ -92,7 +101,7 @@ class FamilyControlModel: ObservableObject {
     func removeDisallowedApps(bundleIdentifiers: [String]) {
         var applicationTokens = store.shield.applications ?? Set<ApplicationToken>()
         for bundleIdentifier in bundleIdentifiers {
-            if let token = ApplicationToken(bundleIdentifier: bundleIdentifier) {
+            if let token = createApplicationToken(bundleIdentifier: bundleIdentifier) {
                 applicationTokens.remove(token)
             }
         }
